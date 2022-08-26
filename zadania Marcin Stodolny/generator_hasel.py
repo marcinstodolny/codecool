dc8ce29ac5acb55fdd91944b2c27375261e15379
Marcin Stodolny <marcin.stodolny@o2.pl>
2022-08-26 11:53:43 +0200

    edited generator and rock_paper_scissors

--- zadania Marcin Stodolny/generator_hasel.py
+++ zadania Marcin Stodolny/generator_hasel.py
@@ -1,24 +1,28 @@
 import string
 import random
 all_characters = string.ascii_lowercase
-lenght = ""
-answer = ["Yes", "No"]
-answer_d = ""
-answer_u = ""
-answer_c = ""
-while answer_d not in answer:
-    answer_d = input("Do you want digits in password?(Yes/No): ")
-while answer_u not in answer:
-    answer_u = input("Do you want upercase in password?(Yes/No): ")
-while answer_c not in answer:
-    answer_c = input("Do you want special charracters in password?(Yes/No): ")
-if answer_c.lower() == "yes":
+answer = ["yes", "no"]
+
+answer_digits = input("Do you want digits in password?(Yes/No): ").lower()
+while answer_digits not in answer:
+    answer_digits = input("You entered wrong value, try again\nDo you want digits in password?(Yes/No): ").lower()
+    
+answer_uppercase = input("Do you want upercase in password?(Yes/No): ").lower()    
+while answer_uppercase not in answer:
+    answer_uppercase = input("You entered wrong value, try again\nDo you want upercase in password?(Yes/No): ").lower()
+    
+answer_special = input("Do you want special charracters in password?(Yes/No): ").lower()
+while answer_special not in answer:
+    answer_special = input("You entered wrong value, try again\nDo you want special charracters in password?(Yes/No): ").lower()
+    
+if answer_special == "yes":
     all_characters += (string.punctuation)
-if answer_u.lower() == "yes":
+if answer_uppercase == "yes":
     all_characters += (string.ascii_uppercase)
-if answer_d.lower() == "yes":
+if answer_digits == "yes":
     all_characters += (string.digits)
+lenght = input("how long?: ")
 while not lenght.isnumeric():
-    lenght = input("how long?: ")
+    lenght = input("You entered wrong value, try again\nhow long?: ")
 password = [random.choice(all_characters) for _ in range(int(lenght))]
-print("".join(password))
+print("Your password: " + "".join(password))
--- zadania Marcin Stodolny/papier_kamien_nozyce.py
+++ zadania Marcin Stodolny/papier_kamien_nozyce.py
@@ -1,11 +1,13 @@
 import random
+
 answer = ["rock", "paper", "scissors"]
 computer_answer = random.choice(answer)
-user_answer = ""
+user_answer = input("rock, paper or scissors?: ").lower()
 while user_answer not in answer:
-    user_answer = input("rock, paper or scissors?: ")
+    print("You entered wrong value, try again")
+    user_answer = input("rock, paper or scissors?: ").lower()
 if computer_answer == user_answer:
-    print("You both chose same thing")
+    print("You both chose same thing - it's a draw")
 elif computer_answer == "rock" and user_answer == "paper" or computer_answer == "paper" and user_answer == "scissors" or computer_answer == "scissors" and user_answer == "rock":
     print("User won")
 else:
